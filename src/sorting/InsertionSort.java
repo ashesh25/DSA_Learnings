bace8cd956a51f90bd647c6c1bee30c15aad56a7
Ashesh Chakraborty <cyber.ashesh@gmail.com>
2026-02-24 09:17:43 +0530

    added insertion sort algo

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {5,2,4,8,3};
+		
+		int n = arr.length;
+		
+		for( int i = 1; i < n; i++ ) {
+			int key = arr[i];
+			int j = i - 1;
+			
+			while( j >= 0 && arr[j]> key ) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
+		
+		for( int i = 0; i < n; i++ ) {
+			System.out.print(arr[i]+ " ");
+		}
+	}
+
+}
