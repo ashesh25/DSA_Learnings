c8a0f50e1f871499d13ace036e51b9fa58e04e14
Ashesh Chakraborty <cyber.ashesh@gmail.com>
2026-07-02 07:09:12 +0530

    add Day_18_FInd_Peak_Element_30_06_26 class for finding a peak element in an array

---
+++ src/misc/Day_18_FInd_Peak_Element_30_06_26.java
@@ -0,0 +1,36 @@
+package misc;
+
+import java.util.Arrays;
+
+public class Day_18_FInd_Peak_Element_30_06_26 {
+    public static void main(String[] args) {
+       int nums[] = {9,7,3,7,8};
+       System.out.println(findPeakElement(nums)); 
+    }
+
+    public static int findPeakElement(int[] nums) {
+        int result = 0;
+        if(nums.length<3){
+            if(nums.length==2){
+                result = nums[1]>nums[0]?1:0;
+            }
+            else if(nums.length==1){
+                result = 0;
+            }
+            return result;
+        }
+        for(int i = 1; i < nums.length - 1; i++){
+            if((nums[i] > nums[i-1]) && nums[i] > nums[i+1])
+                result = i;
+        }
+        if(result == 0){
+            int last = nums[nums.length-1];
+            Arrays.sort(nums);
+            int sortedLast = nums[nums.length-1];
+            if(last == sortedLast){
+                result = nums.length-1;
+            }
+        }
+        return result;
+    }
+}
--- src/priorityQueue/Day_17_Kth_Largest_Element_29_06_26.java
+++ src/priorityQueue/Day_17_Kth_Largest_Element_29_06_26.java
@@ -1,4 +1,4 @@
-package misc;
+package priorityQueue;
 
 import java.util.Arrays;
 import java.util.Iterator;
